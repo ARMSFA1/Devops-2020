6e885535714e84221c06873fdc95f0142fda61cb
ARMSFA1 <51897236+ARMSFA1@users.noreply.github.com>
2020-09-10 15:33:48 +1200

    Update idleTest.ps1

--- idleTest.ps1
+++ idleTest.ps1
@@ -1,21 +1,16 @@
-﻿Add-Type @'
+Add-Type @'
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-
 namespace PInvoke.Win32 {
-
     public static class UserInput {
-
         [DllImport("user32.dll", SetLastError=false)]
         private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
-
         [StructLayout(LayoutKind.Sequential)]
         private struct LASTINPUTINFO {
             public uint cbSize;
             public int dwTime;
         }
-
         public static DateTime LastInput {
             get {
                 DateTime bootTime = DateTime.UtcNow.AddMilliseconds(-Environment.TickCount);
@@ -23,13 +18,11 @@ namespace PInvoke.Win32 {
                 return lastInput;
             }
         }
-
         public static TimeSpan IdleTime {
             get {
                 return DateTime.UtcNow.Subtract(LastInput);
             }
         }
-
         public static int LastInputTicks {
             get {
                 LASTINPUTINFO lii = new LASTINPUTINFO();
@@ -47,12 +40,15 @@ for ( $i = 0; $i -lt 10; $i++ ) {
     $Last = [PInvoke.Win32.UserInput]::LastInput
     $Idle = [PInvoke.Win32.UserInput]::IdleTime
     $LastStr = $Last.ToLocalTime().ToString("MM/dd/yyyy hh:mm tt")
+    $cpuUsage = (Get-WmiObject Win32_Processor | Select LoadPercentage | Format-List | Out-String)
+    $cpuPercent =  $cpuUsage.substring($cpuUsage.length-10) ## this is currently resulting in a bug where extra lines are included which are blank. "$cpuPercent =  ($cpuUsage).substring(17, 20)" possible solution?
     Write-Host ("^<-Start Result-^>")
     Write-Host ("`nTest " + $i)
     Write-Host ("   Last user keyboard/mouse input: " + $LastStr)
+    Write-Host ("   CPU usage: " + $cpuPercent + " %")
     Write-Host ("   Idle for " + $Idle.Days + " days, " + $Idle.Hours + " hours, " + $Idle.Minutes + " minutes, " + $Idle.Seconds + " seconds.")
-      if ($Idle.Seconds -ge 10) {
-        Write-Host (" This machine has been Idle for longer than the alloted time. It will now shutdown.")
+      if ($Idle.Minutes -ge 30) {
+        Write-Host (" This machine has been idle for too long. It will now shutdown.")
         Start-Sleep -Seconds 10
         Stop-Computer
     }
