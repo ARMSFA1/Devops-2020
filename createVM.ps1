0bb44a0a4b030f7f7163088ee460b5bbea83d929
ARMSFA1 <51897236+ARMSFA1@users.noreply.github.com>
2020-09-21 14:54:50 +1200

    Add files via upload

--- createVM.ps1
+++ createVM.ps1
@@ -13,6 +13,7 @@ Echo "==============================="
 Echo ""
 Echo ""
 $RG = 0
+$existingGroup = 0
 
 do {
     $i = Read-Host -Prompt "Are you using an existing resource group? (Y/N)"
@@ -31,6 +32,7 @@ do {
         }while ($RG -eq 0)
     }elseif ($i -eq "N") { ## New Resourse group
         $ResourceGroup = Read-Host -Prompt "What would you like to call your Resource Group (no spaces)"
+        $existingGroup = 1 ## This is used later in the program to show the user the correct data.
         Echo "Please visit https://azure.microsoft.com/en-us/global-infrastructure/geographies/#overview to see region names (Ex. East US would be input as, eastus)"
         $Location = Read-Host -Prompt "Where would you like your Resource Group to be hosted?"
         do {
@@ -52,3 +54,137 @@ do {
     }
 } while ($RG -eq 0)
 
+
+$VirtualNetworkName = Read-Host -Prompt "Please enter your Virtual Network Name" ##storing virtual network name
+do {
+    $i = Read-Host -Prompt ("is " + $VirtualNetworkName + " correct? (Y/N)")
+    if ($i -eq "Y") {
+        echo ("Virtual Network Name confirmed as " + $VirtualNetworkName)
+
+        }elseif ($i -eq "N") {
+        Read-Host -Prompt "Please enter your Virtual Network Name"
+        }else {
+        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+    } 
+   }while ($i -ne "Y")
+
+$SubnetName = Read-Host -Prompt "Please enter your Subnet name" ##storing subnetname
+do {
+    $i = Read-Host -Prompt ("is " + $SubnetName + " correct? (Y/N)")
+    if ($i -eq "Y") {
+        echo ("Subnet name confirmed as " + $SubnetName)
+
+        }elseif ($i -eq "N") {
+        Read-Host -Prompt "Please enter your Subnet name"
+        }else {
+        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+    } 
+   }while ($i -ne "Y")
+
+$SecurityGroupName = Read-Host -Prompt "Please enter your Security Group Name" ##storing securitygroupname
+do {
+    $i = Read-Host -Prompt ("is " + $SecurityGroupName + " correct? (Y/N)")
+    if ($i -eq "Y") {
+        echo ("Security Group Name confirmed as " + $SecurityGroupName)
+
+        }elseif ($i -eq "N") {
+        Read-Host -Prompt "Please enter your Security Group Name"
+        }else {
+        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+    } 
+   }while ($i -ne "Y")
+
+$PublicIPAddress = Read-Host -Prompt "Please enter your Public IP Address" ##storing publicipaddress
+do {
+    $i = Read-Host -Prompt ("is " + $PublicIPAddress + " correct? (Y/N)")
+    if ($i -eq "Y") {
+        echo ("Virtual Network Name confirmed as " + $PublicIPAddress)
+
+        }elseif ($i -eq "N") {
+        Read-Host -Prompt "Please enter your Public IP Address"
+        }else {
+        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+    } 
+   }while ($i -ne "Y")
+
+   Echo "---Compiling Information---"
+   Echo "---------------------------"
+
+   if ($existingGroup -eq 1){
+
+   Echo "Please confirm the following information is correct." ## New ResourceGroup
+   Echo ("1. Resource Group Name " + $ResourceGroup)
+   Echo ("2. Location " + $Location)
+   Echo ("3. Virtual NetworkName " + $VirtualNetworkName)
+   Echo ("4. Subnet Name " + $SubnetName)
+   Echo ("5. Seucrity Group Name " + $SecurityGroupName)
+   Echo ("6. Public IP Address " + $PublicIPAddress)
+   Echo "If everything is fine enter Y"
+   $i = Read-Host -Prompt ("If changes are required enter N")
+
+   if ($i -eq "Y") {
+    Echo "Please wait your data is being confirmed."
+    }elseif ($i -eq "N") {
+    $g = Read-Host -Prompt "Which value is incorrect? 1..7"
+    $h = Read-Host -Prompt "Please enter the new value."
+    switch ($g){
+           1 {$ResourceGroup = $h; Break}
+           2 {$Location = $h; Break}
+           3 {$VirtualNetworkName = $h; Break}
+           4 {$SubnetName = $h; Break}
+           5 {$SecurityGroupName = $h; Break}
+           6 {$PublicIPAddress = $h}
+       }
+
+    $i = Read-Host -Prompt "Are those all the changes you needed? (Y/N)"
+    if ($g -eq "Y"){
+    Echo "Saving changes."
+    }
+    if ($g -eq "N"){
+    Echo "Updating...."
+    }
+    else {
+   Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+   }
+
+   
+   }else {
+
+   Echo "Please confirm the following information is correct." ## Existing ResourceGroup
+   Echo ("1. Resource Group Name " + $ResourceGroup)
+   Echo ("2. Virtual NetworkName " + $VirtualNetworkName)
+   Echo ("3. Subnet Name " + $SubnetName)
+   Echo ("4. Seucrity Group Name " + $SecurityGroupName)
+   Echo ("5. Public IP Address " + $PublicIPAddress)
+   Echo "If everything is fine enter Y"
+   Echo "If changes are required enter N" 
+
+   if ($i -eq "Y") {
+    Echo "Please wait your data is being confirmed."
+    }elseif ($i -eq "N") {
+    $g = Read-Host -Prompt "Which value is incorrect? 1..7"
+    $h = Read-Host -Prompt "Please enter the new value."
+    switch ($g){
+           1 {$ResourceGroup = $h; Break}
+           2 {$VirtualNetworkName = $h; Break}
+           3 {$SubnetName = $h; Break}
+           4 {$SecurityGroupName = $h; Break}
+           5 {$PublicIPAddress = $h}
+       }
+
+    $i = Read-Host -Prompt "Are those all the changes you needed? (Y/N)"
+    if ($g -eq "Y"){
+    Echo "Saving changes."
+    }
+    if ($g -eq "N"){
+    Echo "Updating...."
+    }
+    else {
+   Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+   }
+   }
+        
+
+
+
+            
