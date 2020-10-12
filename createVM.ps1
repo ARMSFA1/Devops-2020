fe47ad2f77795e5a19f4db770cd47b0a62ea5537
ARMSFA1 <51897236+ARMSFA1@users.noreply.github.com>
2020-10-12 14:40:36 +1300

    Add files via upload

--- createVM.ps1
+++ createVM.ps1
@@ -12,48 +12,36 @@ Echo "|  Finn Armstrong McAllister  |"
 Echo "==============================="
 Echo ""
 Echo ""
-$RG = 0
-$existingGroup = 0
 
+$ResourceGroup = Read-Host -Prompt "Please enter the name of your already existing Resource Group" ##storing resource group
 do {
-    $i = Read-Host -Prompt "Are you using an existing resource group? (Y/N)"
+    $i = Read-Host -Prompt ("is " + $ResourceGroup + " correct? (Y/N)")
     if ($i -eq "Y") {
-        $ResourceGroup = Read-Host -Prompt "Please enter your Resource Group name" ## Existing resource group
-        do {
-            $j = Read-Host -Prompt ("Is, " + $ResourceGroup + " correct? (Y/N)") 
-            if ($j -eq "Y") {
-                Echo ("Resource Group confirmed as: " + $ResourceGroup) ## Existing resource group and input is correct
-                $RG = 1
-            }elseif ($i -eq "N") {
-                $ResourceGroup = Read-Host -Prompt "Please enter your Resource Group name" ## Existing resource group and input is incorrect
-            }else {
-                Echo "Please only enter Y for yes, or N for no." ## existing resource group and response is illegible 
-            } 
-        }while ($RG -eq 0)
-    }elseif ($i -eq "N") { ## New Resourse group
-        $ResourceGroup = Read-Host -Prompt "What would you like to call your Resource Group (no spaces)"
-        $existingGroup = 1 ## This is used later in the program to show the user the correct data.
-        Echo "Please visit https://azure.microsoft.com/en-us/global-infrastructure/geographies/#overview to see region names (Ex. East US would be input as, eastus)"
-        $Location = Read-Host -Prompt "Where would you like your Resource Group to be hosted?"
-        do {
-            $j = Read-Host -Prompt ("Is " + $ResourceGroup + " the correct name AND " + $Location + " the correct location? (Y/N)")
-            if ($j -eq "Y") {
-                Echo ("Resource Group confirmed as: " + $ResourceGroup,"Location confirmed as: " + $ResourceGroup) ## New resourse group and input is correct
-                $RG = 1
-                ## create resource group and set location in the future here!
-            }elseif ($i -eq "N") {
-                $ResourceGroup = Read-Host -Prompt "What would you like to call your Resource Group (no spaces)"
-                Echo "Please visit https://azure.microsoft.com/en-us/global-infrastructure/geographies/#overview to see region names (Ex. East US would be input as, eastus)"
-                $Location = Read-Host -Prompt "Where would you like your Resource Group to be hosted?" ## New resourse group and input is incorrect
-            }else {
-                Echo "Please only enter Y for yes, or N for no." ## New resourse group and input is illegible
-            } 
-        }while ($RG -eq 0)
-    }else {
-        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
-    }
-} while ($RG -eq 0)
+        echo ("Resource Group confirmed as " + $ResourceGroup)
 
+    }
+    elseif ($i -eq "N") {
+        Read-Host -Prompt "Please enter your Resource Group"
+    }
+    else {
+        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+    } 
+}while ($i -ne "Y")
+
+$VMName = Read-Host -Prompt "What would you like to call your Virtual Machine?" ##storing VM name
+do {
+    $i = Read-Host -Prompt ("is " + $VMName + " correct? (Y/N)")
+    if ($i -eq "Y") {
+        echo ("Virtual Network Name confirmed as " + $VMName)
+
+    }
+    elseif ($i -eq "N") {
+        Read-Host -Prompt "Please enter your Virtual Machine name"
+    }
+    else {
+        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+    } 
+}while ($i -ne "Y")
 
 $VirtualNetworkName = Read-Host -Prompt "Please enter your Virtual Network Name" ##storing virtual network name
 do {
@@ -61,12 +49,14 @@ do {
     if ($i -eq "Y") {
         echo ("Virtual Network Name confirmed as " + $VirtualNetworkName)
 
-        }elseif ($i -eq "N") {
+    }
+    elseif ($i -eq "N") {
         Read-Host -Prompt "Please enter your Virtual Network Name"
-        }else {
+    }
+    else {
         Echo "Please only enter Y for yes, or N for no." ##  input is illegible
     } 
-   }while ($i -ne "Y")
+}while ($i -ne "Y")
 
 $SubnetName = Read-Host -Prompt "Please enter your Subnet name" ##storing subnetname
 do {
@@ -74,12 +64,14 @@ do {
     if ($i -eq "Y") {
         echo ("Subnet name confirmed as " + $SubnetName)
 
-        }elseif ($i -eq "N") {
+    }
+    elseif ($i -eq "N") {
         Read-Host -Prompt "Please enter your Subnet name"
-        }else {
+    }
+    else {
         Echo "Please only enter Y for yes, or N for no." ##  input is illegible
     } 
-   }while ($i -ne "Y")
+}while ($i -ne "Y")
 
 $SecurityGroupName = Read-Host -Prompt "Please enter your Security Group Name" ##storing securitygroupname
 do {
@@ -87,12 +79,14 @@ do {
     if ($i -eq "Y") {
         echo ("Security Group Name confirmed as " + $SecurityGroupName)
 
-        }elseif ($i -eq "N") {
+    }
+    elseif ($i -eq "N") {
         Read-Host -Prompt "Please enter your Security Group Name"
-        }else {
+    }
+    else {
         Echo "Please only enter Y for yes, or N for no." ##  input is illegible
     } 
-   }while ($i -ne "Y")
+}while ($i -ne "Y")
 
 $PublicIPAddress = Read-Host -Prompt "Please enter your Public IP Address" ##storing publicipaddress
 do {
@@ -100,91 +94,137 @@ do {
     if ($i -eq "Y") {
         echo ("Virtual Network Name confirmed as " + $PublicIPAddress)
 
-        }elseif ($i -eq "N") {
+    }
+    elseif ($i -eq "N") {
         Read-Host -Prompt "Please enter your Public IP Address"
-        }else {
+    }
+    else {
         Echo "Please only enter Y for yes, or N for no." ##  input is illegible
     } 
-   }while ($i -ne "Y")
+}while ($i -ne "Y")
 
-   Echo "---Compiling Information---"
-   Echo "---------------------------"
+Echo "Would you like to add additional open ports? (Y/N)"
+$i = Read-Host -Prompt "By default 22 (SSH), 80 (HTTP) and 443 (HTTPS) are used." ##storing ports. Needs option to ask if they wish to add another port (make open port an array??)
+if ($i -eq "Y") {
+    $OpenPort = Read-Host -Prompt "Please enter the port you wish to open."
+    do {
+        $i = Read-Host -Prompt ("is " + $OpenPort + " correct? (Y/N)")
+        if ($i -eq "Y") {
+            echo ("Open port confirmed as " + $OpenPort)
+        }
+        elseif ($i -eq "N") {
+            Read-Host -Prompt "Please enter your Public IP Address"
+        }
+        else {
+            Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+        } 
+    }while ($i -ne "Y")
+}
+elseif ($i -eq "N") {
+    $OpenPort = 0
+}
 
-   if ($existingGroup -eq 1){
 
-   Echo "Please confirm the following information is correct." ## New ResourceGroup
-   Echo ("1. Resource Group Name " + $ResourceGroup)
-   Echo ("2. Location " + $Location)
-   Echo ("3. Virtual NetworkName " + $VirtualNetworkName)
-   Echo ("4. Subnet Name " + $SubnetName)
-   Echo ("5. Seucrity Group Name " + $SecurityGroupName)
-   Echo ("6. Public IP Address " + $PublicIPAddress)
-   Echo "If everything is fine enter Y"
-   $i = Read-Host -Prompt ("If changes are required enter N")
+Echo "---Compiling Information---"
+Echo "---------------------------"
 
-   if ($i -eq "Y") {
+Echo "Please confirm the following information is correct." ## New ResourceGroup
+Echo ("1. Resource Group Name " + $ResourceGroup)
+Echo ("2. Location " + $Location)
+Echo ("3. Virtual NetworkName " + $VirtualNetworkName)
+Echo ("4. Subnet Name " + $SubnetName)
+Echo ("5. Seucrity Group Name " + $SecurityGroupName)
+Echo ("6. Public IP Address " + $PublicIPAddress)
+Echo "If everything is fine enter Y"
+$i = Read-Host -Prompt ("If changes are required enter N")
+
+if ($i -eq "Y") {
     Echo "Please wait your data is being confirmed."
-    }elseif ($i -eq "N") {
+}
+elseif ($i -eq "N") {
     $g = Read-Host -Prompt "Which value is incorrect? 1..7"
     $h = Read-Host -Prompt "Please enter the new value."
-    switch ($g){
-           1 {$ResourceGroup = $h; Break}
-           2 {$Location = $h; Break}
-           3 {$VirtualNetworkName = $h; Break}
-           4 {$SubnetName = $h; Break}
-           5 {$SecurityGroupName = $h; Break}
-           6 {$PublicIPAddress = $h}
-       }
+    switch ($g) {
+        1 { $ResourceGroup = $h; Break }
+        2 { $Location = $h; Break }
+        3 { $VirtualNetworkName = $h; Break }
+        4 { $SubnetName = $h; Break }
+        5 { $SecurityGroupName = $h; Break }
+        6 { $PublicIPAddress = $h }
+    }
 
     $i = Read-Host -Prompt "Are those all the changes you needed? (Y/N)"
-    if ($g -eq "Y"){
-    Echo "Saving changes."
+    if ($g -eq "Y") {
+        Echo "Saving changes."
     }
-    if ($g -eq "N"){
-    Echo "Updating...."
+    if ($g -eq "N") {
+        Echo "Updating...."
     }
     else {
-   Echo "Please only enter Y for yes, or N for no." ##  input is illegible
-   }
+        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+    }
 
    
-   }else {
+}
+else {
 
-   Echo "Please confirm the following information is correct." ## Existing ResourceGroup
-   Echo ("1. Resource Group Name " + $ResourceGroup)
-   Echo ("2. Virtual NetworkName " + $VirtualNetworkName)
-   Echo ("3. Subnet Name " + $SubnetName)
-   Echo ("4. Seucrity Group Name " + $SecurityGroupName)
-   Echo ("5. Public IP Address " + $PublicIPAddress)
-   Echo "If everything is fine enter Y"
-   Echo "If changes are required enter N" 
+    Echo "Please confirm the following information is correct." ## Existing ResourceGroup
+    Echo ("1. Resource Group : " + $ResourceGroup)
+    Echo ("2. Virtual Machine Name : " + $VMName)
+    Echo ("3. Virtual Network Name: " + $VirtualNetworkName)
+    Echo ("4. Subnet Name: " + $SubnetName)
+    Echo ("5. Seucrity Group Name: " + $SecurityGroupName)
+    Echo ("6. Public IP Address: " + $PublicIPAddress)
+    Echo ("7. Open Ports: 22, 80, 443, " + $OpenPorts)
+    Echo "If everything is fine enter Y"
+    $i = Read-Host -Prompt "If changes are required enter N"
 
-   if ($i -eq "Y") {
-    Echo "Please wait your data is being confirmed."
-    }elseif ($i -eq "N") {
-    $g = Read-Host -Prompt "Which value is incorrect? 1..7"
-    $h = Read-Host -Prompt "Please enter the new value."
-    switch ($g){
-           1 {$ResourceGroup = $h; Break}
-           2 {$VirtualNetworkName = $h; Break}
-           3 {$SubnetName = $h; Break}
-           4 {$SecurityGroupName = $h; Break}
-           5 {$PublicIPAddress = $h}
-       }
-
-    $i = Read-Host -Prompt "Are those all the changes you needed? (Y/N)"
-    if ($g -eq "Y"){
-    Echo "Saving changes."
+    if ($i -eq "Y") {
+        Echo "Please wait your data is being confirmed."
     }
-    if ($g -eq "N"){
-    Echo "Updating...."
+    elseif ($i -eq "N") {
+        $g = Read-Host -Prompt "Which value is incorrect? 1..7"
+        $h = Read-Host -Prompt "Please enter the new value."
+        switch ($g) {
+            1 { $ResourceGroup = $h; Break }
+            2 { $VMName = $h; Break }
+            3 { $VirtualNetworkName = $h; Break }
+            4 { $SubnetName = $h; Break }
+            5 { $SecurityGroupName = $h; Break }
+            6 { $PublicIPAddress = $h; Break }
+            7 { $OpenPorts = $h; Break }
+
+        }
+
+        $i = Read-Host -Prompt "Are those all the changes you needed? (Y/N)"
+        if ($g -eq "Y") {
+            Echo "Saving changes."
+        }
+        if ($g -eq "N") {
+            Echo "Updating...."
+        }
+        else {
+            Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+        }
     }
+
     else {
-   Echo "Please only enter Y for yes, or N for no." ##  input is illegible
-   }
-   }
-        
+        Echo "Please only enter Y for yes, or N for no." ##  input is illegible
+    }
+}
+
+Echo "---Creating Virtual Machine---"
+Echo "------------------------------"
+
+New-AzVm `
+    -ResourceGroupName $ResourceGroup
+-Name $VMName
+-Location $location 
+-VirtualNetworkName $VirtualNetworkName
+-SubnetName $SubnetName
+-SecurityGroupName $SecurityGroupName
+-PublicIpAddressName $PublicIPAddress
+-OpenPorts 22, 80, 443, $OpenPort
 
 
-
-            
+     
